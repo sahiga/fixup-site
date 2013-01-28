3e0d2851d928b11e240760457702c70e70f4cf36
Stephanie <stephanie@curiousminds.com>
2013-01-28 15:35:27 -0800

    Added Google Analytics tracking code

--- php/functions.php
+++ php/functions.php
@@ -37,6 +37,8 @@
 	remove_action( 'wp_head', 'wp_generator' );
 	
 	add_action( 'wp_enqueue_scripts', 'fixup_script_enqueuer' );
+	
+	add_action( 'wp_footer', 'google_analytics' );
 
 	add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
 
@@ -88,6 +90,27 @@
 		}
 		
 	}
+	
+	// Include Google Analytics Tracking Code
+	
+	function google_analytics() {
+		$propertyID = 'UA-36454513-1'; // GAP Property ID
+		?>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', '<?php echo $propertyID; ?>']);
+			_gaq.push(['_setDomainName', 'tryfixup.com']);
+			_gaq.push(['_trackPageview']);
+
+  			(function() {
+    			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    		})();
+    	</script>
+   		<?php
+    }
+	
 
 	/* ========================================================================================================================
 	
